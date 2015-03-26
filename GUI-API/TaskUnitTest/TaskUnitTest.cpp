9ad8ed9aa1bf315ab5c51d9c1f71f0ba0a08864f
Karthikeyan <karthikeyan.shanmugam25@gmail.com>
2015-03-27 01:05:38 +0800

    [v0.2] added more unit tests

--- GUI-API/TaskUnitTest/TaskUnitTest.cpp
+++ GUI-API/TaskUnitTest/TaskUnitTest.cpp
@@ -10,39 +10,48 @@ namespace TaskUnitTest
 	public:
 		
 		TEST_METHOD(addDetailsTest){
-			Task taskTwoDatesTwoTimes, taskNoDateNoTime, taskOneDateOneTime, taskOneDateNoTime, taskNoDateTwoTimes;
+			Task testTask1, testTask2, testTask3;
 			bool isDescCorrect = false, isTimeCorrect = false, isStartDateCorrect = false, isEndDateCorrect = false;
 		
-			taskTwoDatesTwoTimes.addDetails("Meeting in school; date 270315 to 290315; time 1300 to 1500");
-			
-			/*
-			taskNoDateNoTime.addDetails("add Do CS Assignment");
-			taskOneDateOneTime.addDetails("Meeting with Prof; date 270315; time 1400");
-			taskOneDateNoTime.addDetails("Math Assignment; date 010415");
-			taskNoDateTwoTimes.addDetails("Meet girlfriend for dinner; 1800 to 2030");
-			*/
+			testTask1.addDetails("Meeting in school; date 270315 to 290315; time 1300 to 1500");
+			testTask2.addDetails("Project meeting; date 280315; time 1400");
+			testTask3.addDetails("Consultation with prof");
 
 			//Check Description
-			if (taskTwoDatesTwoTimes.getDescription() == "Meeting in school"){
+			if (testTask1.getDescription() == "Meeting in school"){
 				isDescCorrect = true;
 			}
 			Assert::IsTrue(isDescCorrect);
 			
 			//Check Time
-			if (taskTwoDatesTwoTimes.getTimeStart() == 1300 && taskTwoDatesTwoTimes.getTimeEnd() == 1500){
+			//Equivalence partition [0 .. 2] is the range that produces true
+			//Check boundary case 0, 1, 2 and 3
+			Assert::AreNotEqual(3, testTask1.getNumOfTimes());
+			Assert::AreEqual(2, testTask1.getNumOfTimes());
+			Assert::AreEqual(1, testTask2.getNumOfTimes());
+			Assert::AreEqual(0, testTask3.getNumOfTimes());
+
+			if (testTask1.getTimeStart() == 1300 && testTask1.getTimeEnd() == 1500){
 				isTimeCorrect = true;
 			}
 			Assert::IsTrue(isTimeCorrect);
 
 			//Check Date
-			if (taskTwoDatesTwoTimes.getDateStart().day == 27 && taskTwoDatesTwoTimes.getDateStart().month == 3 && 
-					taskTwoDatesTwoTimes.getDateStart().year == 15){
+			//Equivalence partition [0 .. 2] is the range that produces true
+			//Check boundary case 0, 1, 2 and 3
+			Assert::AreNotEqual(3, testTask1.getNumOfDates());
+			Assert::AreEqual(2, testTask1.getNumOfDates());
+			Assert::AreEqual(1, testTask2.getNumOfDates());
+			Assert::AreEqual(0, testTask3.getNumOfDates());
+
+			if (testTask1.getDateStart().day == 27 && testTask1.getDateStart().month == 3 && 
+					testTask1.getDateStart().year == 15){
 				isStartDateCorrect = true;
 			}
 			Assert::IsTrue(isStartDateCorrect);
 
-			if (taskTwoDatesTwoTimes.getDateEnd().day == 29 && taskTwoDatesTwoTimes.getDateEnd().month == 3 && 
-					taskTwoDatesTwoTimes.getDateEnd().year == 15){
+			if (testTask1.getDateEnd().day == 29 && testTask1.getDateEnd().month == 3 && 
+					testTask1.getDateEnd().year == 15){
 				isEndDateCorrect = true;
 			}
 			Assert::IsTrue(isEndDateCorrect);
